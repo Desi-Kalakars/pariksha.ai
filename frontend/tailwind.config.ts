34726e80ddc1f01254554a2ac66c9d0c51be0084
PriyaGupta1996 <priyagupta.ynr@gmail.com>
2024-08-05 00:02:19 +0530

    Set up NextJs, tailwind and navigation. 🚀

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors: {
+      'blue-500': '#0752a2',
+      'blue-700':'#2d84e1',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'white':'#ffffff'
+
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+  },
+  plugins: [],
+};
+export default config;
